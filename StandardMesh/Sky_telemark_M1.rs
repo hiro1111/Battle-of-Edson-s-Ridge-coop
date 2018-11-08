a7d99e4d9f31bfd086bb79d60bc765b85f01b6c3
hiro1111 <hiro111197@gmail.com>
2018-11-08 18:50:13 +0900

    add new file

---
+++ StandardMesh/Sky_telemark_M1.rs
@@ -0,0 +1,36 @@
+subshader "Sky_cassino_M1_Material0" "StandardMesh/Default"
+{
+	lighting false;
+	texture "texture/Sky_telemark_05";
+}
+
+subshader "Sky_cassino_M1_Material1" "StandardMesh/Default"
+{
+	lighting false;
+	texture "texture/Sky_telemark_06";
+}
+
+subshader "Sky_cassino_M1_Material2" "StandardMesh/Default"
+{
+	lighting false;
+	texture "texture/Sky_telemark_01";
+}
+
+subshader "Sky_cassino_M1_Material3" "StandardMesh/Default"
+{
+	lighting false;
+	texture "texture/Sky_telemark_02";
+}
+
+subshader "Sky_cassino_M1_Material4" "StandardMesh/Default"
+{
+	lighting false;
+	texture "texture/Sky_telemark_03";
+}
+
+subshader "Sky_cassino_M1_Material5" "StandardMesh/Default"
+{
+	lighting false;
+	texture "texture/Sky_telemark_04";
+}
+
